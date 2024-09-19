11ca115c13dc8cddff520907f90a9179b78ef262
anirudh-248 <anirudhphaniraj1@gmail.com>
2024-09-19 23:02:57 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -11,4 +11,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-if [[-z $CREATE_SUPERUSER]]; then python manage.py createsuperuserfi
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
